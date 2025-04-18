64337621d9e78c697429aa9f5b89057ab6d648f8
HaroldRumiche <158658941+HaroldRumiche@users.noreply.github.com>
2025-04-18 13:44:12 -0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://HaroldRumiche.github.io/',
+  url: 'https://969732197.senati.chat.pe',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: 'semana-9',
